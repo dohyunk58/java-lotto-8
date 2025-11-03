20212ae153f244f3ce1c63de83c7a8d49070f6d6
dohyunk58 <hedge3x@gmail.com>
2025-11-03 23:44:34 +0900

    refactor: Stream으로 Enum 상수 탐색하도록 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,45 +1,39 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public enum LottoRank {
-    FIRST(2_000_000_000L, "6개 일치"),
-    SECOND(30_000_000L, "5개 일치, 보너스 볼 일치"),
-    THIRD(1_500_000L, "5개 일치"),
-    FOURTH(50_000L, "4개 일치"),
-    FIFTH(5_000L, "3개 일치"),
-    MISS(0L, "");
+    FIRST(2_000_000_000L, "6개 일치", 6, false),
+    SECOND(30_000_000L, "5개 일치, 보너스 볼 일치", 5, true),
+    THIRD(1_500_000L, "5개 일치", 5, false),
+    FOURTH(50_000L, "4개 일치", 4, false),
+    FIFTH(5_000L, "3개 일치", 3, false),
+    MISS(0L, "낙첨", 0, false);
 
     private final long prizeMoney;
     private final String description;
+    private final int matchCount;
+    private final boolean bonusRequired;
 
-    LottoRank(long prizeMoney, String description) {
+    LottoRank(long prizeMoney, String description, int matchCount, boolean bonusRequired) {
         this.prizeMoney = prizeMoney;
         this.description = description;
+        this.matchCount = matchCount;
+        this.bonusRequired = bonusRequired;
     }
 
     public static LottoRank of(int matchCount, boolean hasBonus) {
-        if (matchCount == 6) {
-            return FIRST;
-        }
-
+        // 2등은 5개, 보너스 true로 유일한 케이스
         if (matchCount == 5 && hasBonus) {
             return SECOND;
         }
 
-        if (matchCount == 5) {
-            return THIRD;
-        }
-
-        if (matchCount == 4) {
-            return FOURTH;
-        }
-
-        if (matchCount == 3) {
-            return FIFTH;
-        }
-
-        return MISS;
+        return Arrays.stream(values())
+                .filter(rank -> !rank.bonusRequired) // 보너스 필요 없는 등수
+                .filter(rank -> rank.matchCount == matchCount) // 일치 개수가 같은 것
+                .findFirst()
+                .orElse(MISS); // 찾으면 반환 못 찾으면 MISS
     }
 
     public static List<LottoRank> getRanks() {

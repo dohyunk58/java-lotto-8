dc4002ee17a969d5d54fc316a94abef84227dcd1
dohyunk58 <hedge3x@gmail.com>
2025-11-03 16:56:39 +0900

    feat: 당첨 번호 입력값 쉼표로 나눌 수 없는 예외 처리 추가
    입력받은 문자열을 split하기 전 공백을 포함하거나 빈 문자열, null인지 확인하고 구분자(쉼표)로 끝나는 경우에도 예외를 발생시킨다.

--- src/test/java/lotto/domain/ValidatorTest.java
+++ src/test/java/lotto/domain/ValidatorTest.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ValidatorTest {
 
+    // 구매 금액 입력값 검증
+
     @DisplayName("유효한 1000원단위의 양수는 예외를 발생시키지 않는다.")
     @Test
     void validatePurchaseAmount_Success() {
@@ -43,4 +45,34 @@ public class ValidatorTest {
         assertThatThrownBy(() -> Validator.validatePurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    // 당첨 번호 입력값 검증
+
+    @DisplayName("유효한 당첨 번호(6개, 쉼표 구분)는 예외를 발생시키지 않는다.")
+    @Test
+    void validateWinningNumbers_Success() {
+        String winningNumbers = "1,2,3,4,5,6";
+
+        assertDoesNotThrow(() -> {
+            Validator.validateWinningNumbers(winningNumbers);
+        });
+    }
+
+    @DisplayName("당첨 번호가 6개가 아닌 경우 IllegalArgumentException을 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7"})
+    void validateWinningNumbers_InvalidCount(String input) {
+        assertThatThrownBy(() -> Validator.validateWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호 형식이 올바르지 않은 경우(빈 값, 공백, 쉼표로 끝남) IllegalArgumentException을 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "1,2,3,4,5,"})
+    void validateWinningNumbers_InvalidFormat(String input) {
+        assertThatThrownBy(() -> Validator.validateWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
